5ff0c2a9ef5e0866278e2dee1e2a93f43c1f29b0
JangGusWjd <jjanghj3624@gmail.com>
2023-08-15 00:09:07 +0900

    Update EduContentPage.js : 교육 컨텐츠 상세보기 페이지 api 연결 및 태그에 따른 검색 연결

--- src/pages/EduContentPage/EduContentPage.js
+++ src/pages/EduContentPage/EduContentPage.js
@@ -1,66 +1,100 @@
-import React, { useState } from "react";
-import { useParams } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { useParams, useNavigate } from "react-router-dom";
+import axios from "axios";
 import "../../styles/EduContentPage/EduContentPage.scss";
 
-import { useRecoilValue } from "recoil";
-import { educationListState } from "../../Data/EducationContent";
-
-import img1 from "../../Data/EduImg/1.jpeg";
-import img2 from "../../Data/EduImg/2.jpeg";
-import img3 from "../../Data/EduImg/3.jpeg";
-import img4 from "../../Data/EduImg/4.jpeg";
-
 function EducationContentPage() {
-  const { id } = useParams();
-  const educationList = useRecoilValue(educationListState);
-  const images = [img1, img2, img3, img4];
+  const [educationContent, setEducationContent] = useState({
+    contentPageList: [],
+  });
+  const { contentId } = useParams();
+  const navigate = useNavigate();
 
-  const content = educationList.find((item) => item.id === parseInt(id, 10));
-  //api 호출 가능
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          `http://13.209.49.229:8080/api/v1/content/${contentId}`
+        );
+        setEducationContent(response.data.result);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, [contentId]);
+
   const [currentIndex, setCurrentIndex] = useState(0); // 현재 이미지 인덱스
 
-  const imageDescriptions = [
-    [
-      "원하는 주문을 할 매장을 선택해야 합니다.",
-      "아래와 같은 화면에서 배달을 시킬 매장을 선택해주세요!",
-    ],
-    [
-      "매장에 존재하는 메뉴의 옵션들을 모두 선택한 뒤, 장바구니 담기를 클릭하세요!",
-    ],
-    ["원하시는 메뉴가 맞는지 확인하고, 배달 주문하기를 클릭하세요!"],
-    [
-      "주문 내역을 확인하였으면 결제 수단을 설정합니다.",
-      "아래 화면에서 결제 수단을 정하고 <결제하기> 버튼을 눌러주세요!",
-    ],
-  ];
-
-  if (!content) {
+  if (!educationContent || !educationContent.contentPageList.length) {
     return <div>컨텐츠를 읽을 수 없습니다.</div>;
   }
 
   const prevButtonIcon =
     currentIndex === 0 ? "before.svg" : "before-active.svg";
   const nextButtonIcon =
-    currentIndex === images.length - 1 ? "next.svg" : "next-active.svg";
+    currentIndex === educationContent.contentPageList.length - 1
+      ? "next.svg"
+      : "next-active.svg";
 
   const handlePrev = () => {
     setCurrentIndex(
-      (prevIndex) => (prevIndex - 1 + images.length) % images.length
+      (prevIndex) =>
+        (prevIndex - 1 + educationContent.contentPageList.length) %
+        educationContent.contentPageList.length
     );
   };
 
   const handleNext = () => {
-    setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
+    setCurrentIndex(
+      (prevIndex) => (prevIndex + 1) % educationContent.contentPageList.length
+    );
+  };
+
+  const currentPage = educationContent.contentPageList[currentIndex];
+
+  const goSearchApp = (tag) => {
+    if (tag.trim() !== "") {
+      navigate(`/search?q=${encodeURIComponent(tag)}`);
+    }
   };
 
   return (
     <div className="edu-container">
-      <h1>[{content.company}]</h1>
-      <h3>{content.title}</h3>
+      <h1>[{educationContent.companyName}]</h1>
+      <h3>{educationContent.title}</h3>
       <div className="image-count">
         <span>
-          {currentIndex + 1} / {images.length}
+          {currentIndex + 1} / {educationContent.contentPageList.length}
         </span>
+        {currentPage.tagList[0].length > 0 && (
+          <div className="tag-list">
+            <div>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="19"
+                height="19"
+                viewBox="0 0 19 19"
+                fill="none"
+              >
+                <path
+                  d="M1.625 9.5H2.5M9.5 1.625V2.5M16.5 9.5H17.375M3.9 3.9L4.5125 4.5125M15.1 3.9L14.4875 4.5125M7.4875 13.875H11.5125M6.875 13C6.14041 12.4491 5.59779 11.681 5.324 10.8045C5.05022 9.92801 5.05914 8.98762 5.34951 8.1165C5.63988 7.24539 6.19698 6.48772 6.94189 5.95082C7.6868 5.41391 8.58176 5.125 9.5 5.125C10.4182 5.125 11.3132 5.41391 12.0581 5.95082C12.803 6.48772 13.3601 7.24539 13.6505 8.1165C13.9409 8.98762 13.9498 9.92801 13.676 10.8045C13.4022 11.681 12.8596 12.4491 12.125 13C11.7834 13.3382 11.5261 13.752 11.3741 14.208C11.2221 14.6641 11.1796 15.1495 11.25 15.625C11.25 16.0891 11.0656 16.5342 10.7374 16.8624C10.4092 17.1906 9.96413 17.375 9.5 17.375C9.03587 17.375 8.59075 17.1906 8.26256 16.8624C7.93437 16.5342 7.75 16.0891 7.75 15.625C7.8204 15.1495 7.77787 14.6641 7.62586 14.208C7.47385 13.752 7.21663 13.3382 6.875 13Z"
+                  stroke="#FFD233"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />
+              </svg>
+              <p>{currentPage.tagList[0]} 방법을 모른다면?</p>
+            </div>
+            {currentPage.tagList.map((tag, index) => (
+              <button key={index} onClick={() => goSearchApp(tag)}>
+                "{tag}" 바로가기
+              </button>
+            ))}
+          </div>
+        )}
       </div>
       <div className="edu-contents">
         <button
@@ -72,20 +106,24 @@ function EducationContentPage() {
         </button>
         <div className="edu-img">
           <div className="image-description">
-            {imageDescriptions[currentIndex].map((line, idx) => (
-              <p key={idx}>{line}</p>
-            ))}
+            <p>{currentPage.description}</p>
+            <img
+              src={currentPage.url}
+              alt={`${currentPage.pageOrder} img`}
+              className="content-image"
+            />
           </div>
-          <img
-            src={images[currentIndex]}
-            alt={`이미지 ${currentIndex + 1}`}
-            className="content-image"
-          />
         </div>
         <button
-          onClick={currentIndex === images.length - 1 ? null : handleNext}
+          onClick={
+            currentIndex === educationContent.contentPageList.length - 1
+              ? null
+              : handleNext
+          }
           className="img-button"
-          disabled={currentIndex === images.length - 1}
+          disabled={
+            currentIndex === educationContent.contentPageList.length - 1
+          }
         >
           <img src={`/icon/${nextButtonIcon}`} alt="next" />
         </button>
