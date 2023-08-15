e9f70065f7ee522cd6a55a5a8cd0e55134fa5b4e
JangGusWjd <jjanghj3624@gmail.com>
2023-08-15 23:03:05 +0900

    Added QaDetail.js : 문의글 상세보기 컴포넌트 구현

---
+++ src/pages/Q&aPage/QaDetail.js
@@ -0,0 +1,49 @@
+import { useState, useEffect } from "react";
+import { useParams, Link } from "react-router-dom";
+import axios from "axios";
+import QaAll from "./QaAll";
+import "../../styles/Q&aPage/QaDetail.scss";
+
+const QaDetail = () => {
+  const [ask, setAsk] = useState([]);
+  const { inquiryId } = useParams();
+
+  useEffect(() => {
+    const fetchAskData = async () => {
+      try {
+        const response = await axios.get(
+          `http://13.209.49.229:8080/api/v1/inquiry/${inquiryId}`
+        );
+        setAsk(response.data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchAskData();
+  }, [inquiryId]);
+
+  return (
+    <div className="qadetail-container">
+      <Link to="/QaPage">
+        <button>목록으로</button>
+      </Link>
+      <div className="ask-container">
+        <div>
+          <h1>{ask.title}</h1>
+          <p
+            className={
+              ask.isAnswer === "Y" ? "status-complete" : "status-ongoing"
+            }
+          >
+            {ask.isAnswer === "Y" ? "답변 완료" : "진행 중"}
+          </p>
+        </div>
+        <p>{ask.description}</p>
+      </div>
+      <QaAll />
+    </div>
+  );
+};
+
+export default QaDetail;
