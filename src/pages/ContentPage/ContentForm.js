704ca86703d150f1719bbbaa80e0d711a12fc6c5
JangGusWjd <jjanghj3624@gmail.com>
2023-08-16 23:22:49 +0900

    Update ContentForm.js : 컨텐츠 생성 api 연결

--- src/pages/ContentPage/ContentForm.js
+++ src/pages/ContentPage/ContentForm.js
@@ -1,10 +1,16 @@
 import { useState } from "react";
+import { useRecoilValue } from "recoil";
+import axios from "axios";
 import categories from "../../Data/Category";
+import { userState1 } from "../../pages/LoginPage/state";
 import "../../styles/ContentPage/ContentForm.scss";
 
 const ContentForm = () => {
+  const user = useRecoilValue(userState1); //실제 유저 데이터
   const [selectedImage, setSelectedImage] = useState(null); // 기업 로고
   const [isShortEvent, setIsShortEvent] = useState(false); // 단기 이벤트인지
+  const [deadlineDate, setDeadlineDate] = useState(""); // 마감 날짜
+  const [deadlineTime, setDeadlineTime] = useState(""); // 마감 시간
   const [stepCount, setStepCount] = useState(2); // 초기 단계 수
   const [steps, setSteps] = useState([
     { content: "", file: null, hashtags: [] },
@@ -39,25 +45,74 @@ const ContentForm = () => {
     setStepFiles(newStepFiles);
   };
 
-  const handleSubmit = (e) => {
+  // shortYn 값 설정
+  const shortYnValue = isShortEvent ? "Y" : "N";
+  let formattedDeadline = null;
+  if (isShortEvent && deadlineDate && deadlineTime) {
+    // 마감 날짜와 시간을 문자열에서 Date 객체로 변환
+    const formattedDeadlineDate = new Date(deadlineDate);
+    const formattedDeadlineTime = new Date(`1970-01-01T${deadlineTime}:00`);
+    // 마감시간을 "YYYY-MM-DDTHH:MM:SS" 형식으로 조립
+    formattedDeadline = `${formattedDeadlineDate
+      .toISOString()
+      .slice(0, 10)}T${formattedDeadlineTime
+      .getHours()
+      .toString()
+      .padStart(2, "0")}:${formattedDeadlineTime
+      .getMinutes()
+      .toString()
+      .padStart(2, "0")}:00`;
+  }
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    const formData = {
-      title: e.target.title.value,
-      company: e.target.company.value,
-      category: e.target.category.value,
-      logo: selectedImage,
-      steps: steps.map((step, index) => ({
-        content: step.content,
-        file: stepFiles[index], // 각 단계별 파일 추가
-        hashtags: hashTags[index],
-      })),
-      deadline: isShortEvent ? e.target.deadline.value : null,
-    };
-    console.log("제출된 데이터:", formData);
+
+    const formData = new FormData(); // FormData 객체 생성
+
+    formData.append("userId", user.userId);
+    formData.append("title", e.target.title.value);
+    formData.append("description", e.target.description.value);
+    formData.append("companyName", e.target.company.value);
+    formData.append("category", e.target.category.value);
+    formData.append("uploadFile", selectedImage);
+
+    // contentPageList 정보를 FormData에 추가
+    steps.forEach((step, index) => {
+      formData.append(`contentPageList[${index}].pageOrder`, index + 1);
+      formData.append(`contentPageList[${index}].description`, step.content);
+      formData.append(`contentPageList[${index}].uploadFile`, stepFiles[index]);
+      formData.append(`contentPageList[${index}].tagList`, hashTags[index]);
+    });
+
+    formData.append("shortYn", shortYnValue);
+    if (isShortEvent && deadlineDate && deadlineTime) {
+      formData.append("deadLine", formattedDeadline);
+    }
+
+    try {
+      const response = await axios.post(
+        "http://13.209.49.229:8080/api/v1/content",
+        formData,
+        {
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
+        }
+      );
+      console.log("서버 응답:", response.data);
+      console.log("제출된 데이터:", formData);
+      // 성공적인 응답 처리
+    } catch (error) {
+      console.error("데이터 전송 실패", error);
+      console.log(formData);
+      // 네트워크 또는 처리 오류 처리
+    }
   };
 
-  const [inputHashTag, setInputHashTag] = useState("");
   const [hashTags, setHashTags] = useState(new Array(stepCount).fill([]));
+  const [inputHashTag, setInputHashTag] = useState(
+    new Array(stepCount).fill("")
+  );
 
   const addHashTag = (e, index) => {
     const allowedCommand = ["Comma", "Enter", "Space", "NumpadEnter"];
@@ -84,7 +139,12 @@ const ContentForm = () => {
       return newStepHashTags;
     });
 
-    setInputHashTag("");
+    // setInputHashTag("");
+    setInputHashTag((prevInputHashTag) => {
+      const newInputHashTag = [...prevInputHashTag];
+      newInputHashTag[index] = "";
+      return newInputHashTag;
+    });
   };
 
   const keyDownHandler = (e) => {
@@ -99,11 +159,32 @@ const ContentForm = () => {
 
   const changeHashTagInput = (e, index) => {
     setInputHashTag((prevInputHashTag) => {
-      return { ...prevInputHashTag, [index]: e.target.value };
+      const newInputHashTag = [...prevInputHashTag];
+      newInputHashTag[index] = e.target.value;
+      return newInputHashTag;
     });
-
-    // setInputHashTag(e.target.value);
   };
+
+  const isLoggedIn = user && user.userId; // userId 값이 있는지 확인
+  if (!isLoggedIn) {
+    return (
+      <div className="warning-container">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="100"
+          height="100"
+          viewBox="0 0 100 100"
+          fill="none"
+        >
+          <path
+            d="M60.831 9.7499C63.0754 7.50633 66.1191 6.24624 69.2926 6.24683C72.4661 6.24741 75.5093 7.50863 77.7529 9.75302C79.9965 11.9974 81.2565 15.0411 81.256 18.2146C81.2554 21.3881 79.9942 24.4313 77.7498 26.6749L73.1748 31.2499L56.2498 14.3311L60.831 9.75615V9.7499ZM51.831 18.7499L14.6123 55.9686C13.4822 57.0992 12.6306 58.4772 12.1248 59.9937L6.41226 77.1374C6.2282 77.6879 6.20109 78.2787 6.33396 78.8437C6.46682 79.4087 6.75442 79.9256 7.16452 80.3363C7.57462 80.7471 8.09101 81.0355 8.65581 81.1693C9.22061 81.303 9.81151 81.2768 10.3623 81.0937L27.506 75.3749C29.0248 74.8749 30.3998 74.0186 31.531 72.8874L31.9373 72.4874C30.9414 67.5984 31.0219 62.5511 32.1729 57.6963C33.324 52.8414 35.5181 48.2954 38.6029 44.3738C41.6877 40.4523 45.5892 37.2493 50.0364 34.9872C54.4836 32.7252 59.37 31.4585 64.356 31.2749L51.831 18.7499ZM37.4998 65.6249C37.4998 69.3183 38.2272 72.9756 39.6407 76.3879C41.0541 79.8002 43.1257 82.9006 45.7374 85.5123C48.349 88.1239 51.4495 90.1956 54.8618 91.609C58.2741 93.0224 61.9313 93.7499 65.6248 93.7499C69.3182 93.7499 72.9755 93.0224 76.3877 91.609C79.8 90.1956 82.9005 88.1239 85.5121 85.5123C88.1238 82.9006 90.1955 79.8002 91.6089 76.3879C93.0223 72.9756 93.7498 69.3183 93.7498 65.6249C93.7498 58.1657 90.7866 51.012 85.5121 45.7375C80.2377 40.4631 73.084 37.4999 65.6248 37.4999C58.1655 37.4999 51.0118 40.4631 45.7374 45.7375C40.4629 51.012 37.4998 58.1657 37.4998 65.6249ZM43.7498 65.6249C43.7498 61.5651 44.8795 57.5854 47.0127 54.1312C49.1458 50.6769 52.1981 47.8845 55.828 46.0663C59.4579 44.2481 63.5221 43.4759 67.5659 43.8362C71.6097 44.1964 75.4734 45.6749 78.7248 48.1061L48.0998 78.7312C45.2662 74.9501 43.7394 70.3499 43.7498 65.6249ZM65.6248 87.4999C60.7123 87.4999 56.1748 85.8749 52.5248 83.1437L83.1498 52.5186C85.5831 55.7704 87.0632 59.6353 87.4241 63.6806C87.7851 67.7259 87.0128 71.7919 85.1936 75.4231C83.3745 79.0543 80.5803 82.1073 77.1241 84.2403C73.6679 86.3733 69.6861 87.5019 65.6248 87.4999Z"
+            fill="#F24E1E"
+          />
+        </svg>
+        <h1 className="warning-message">로그인 후 이용 가능합니다.</h1>
+      </div>
+    );
+  }
 
   return (
     <div className="content-form-container">
@@ -114,6 +195,10 @@ const ContentForm = () => {
             <label>
               제목&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
               <input type="text" name="title" />
+            </label>
+            <label>
+              교육내용
+              <input type="text" name="description" />
             </label>
             <label>
               기업명 &nbsp;
@@ -250,7 +335,23 @@ const ContentForm = () => {
               />
               단기성 이벤트
             </label>
-            {isShortEvent && <input type="date" name="deadline" />}
+            {/* {isShortEvent && <input type="date" name="deadline" />} */}
+            {isShortEvent && (
+              <div>
+                <input
+                  type="date"
+                  name="deadlineDate"
+                  value={deadlineDate}
+                  onChange={(e) => setDeadlineDate(e.target.value)}
+                />
+                <input
+                  type="time"
+                  name="deadlineTime"
+                  value={deadlineTime}
+                  onChange={(e) => setDeadlineTime(e.target.value)}
+                />
+              </div>
+            )}
           </div>
         </div>
         <div className="content-privacy">
