f2191092dce9748ec6b57432d9eae2e48f748c4f
JangGusWjd <jjanghj3624@gmail.com>
2023-08-17 16:32:04 +0900

    Update MyAsk.js : 마이페이지 문의글 api 연결

--- src/pages/MyPage/MyAsk.js
+++ src/pages/MyPage/MyAsk.js
@@ -1,7 +1,10 @@
 // 내가 작성한 문의글 보기
-import { useRecoilValue } from "recoil";
-import { asksState } from "../../Data/User";
-import { useState } from "react";
+import { useRecoilValue, useRecoilState } from "recoil";
+import { userState1 } from "../../Data/state";
+import { asksState } from "../../Data/state";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 import "../../styles/MyPage/MyAsk.scss";
 
 const TableHeader = () => {
@@ -17,10 +20,10 @@ const TableHeader = () => {
   );
 };
 
-const TableRow = ({ id, title, content, status }) => {
+const TableRow = ({ id, title, content, status, onClick }) => {
   const statusColor = status === "진행 중" ? "#F24E1E" : "#699BF7";
   return (
-    <tr>
+    <tr onClick={onClick}>
       <td>{id}</td>
       <td>{title}</td>
       <td>{content}</td>
@@ -30,12 +33,44 @@ const TableRow = ({ id, title, content, status }) => {
 };
 
 const MyAsk = () => {
-  const asks = useRecoilValue(asksState);
+  const user = useRecoilValue(userState1); // Recoil로부터 user 값을 가져옴
+  const token = user.token; // user 객체에서 token 값을 가져옴
+  const [asks, setAsks] = useRecoilState(asksState);
+
+  useEffect(() => {
+    const config = {
+      // axios 요청 설정을 정의
+      headers: {
+        Authorization: "Bearer " + token, // 토큰을 헤더에 첨부
+      },
+    };
+    // console.log(config);
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          // 사용자가 작성한 게시글 불러오는 걸로 api 경로 수정
+          "http://13.209.49.229:8080/api/v1/inquiry/user",
+          config
+        );
+        setAsks(response.data);
+      } catch (error) {
+        alert("회원 문의글 가져오기 실패");
+      }
+    };
+
+    fetchData();
+  }, []);
+
   const [showAll, setShowAll] = useState(false);
   const displayedAsks = showAll ? asks : asks.slice(0, 5);
 
   const handleToggleShowAll = () => {
     setShowAll(!showAll);
+  };
+
+  const navigate = useNavigate();
+  const goDetail = (inquiryId) => {
+    navigate(`/QaPage/${inquiryId}`);
   };
 
   return (
@@ -46,11 +81,12 @@ const MyAsk = () => {
         <tbody>
           {displayedAsks.map((ask) => (
             <TableRow
-              key={ask.id}
-              id={ask.id}
+              key={ask.inquiryId}
+              id={ask.inquiryId}
               title={ask.title}
-              content={ask.content}
-              status={ask.status}
+              content={ask.description}
+              status={ask.isAnswer === "Y" ? "답변 완료" : "진행 중"}
+              onClick={() => goDetail(ask.inquiryId)}
             />
           ))}
         </tbody>
