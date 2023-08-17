29abe5a0b4cb0d527e7e59a7210c3269ae0f783f
박서영(Tessa) <lilianapsy@naver.com>
2023-08-18 00:48:50 +0900

    Update SearchApp.js: 마감시간 이후 이벤트 공지 수정

--- src/pages/SearchPage/SearchApp.js
+++ src/pages/SearchPage/SearchApp.js
@@ -25,15 +25,23 @@ const SearchApp = () => {
     fetchData();
   }, [searchKeyword]);
 
-  // 마감 시간까지 남은 시간을 계산하는 함수
+// 마감 시간까지 남은 시간을 계산하는 함수
   const getTimeRemaining = (deadline) => {
     const timeLeft = new Date(deadline) - new Date();
     const days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
     const hours = Math.floor(
       (timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
-    );
+  );
+    const minutes = Math.floor(
+      (timeLeft % (1000 * 60 * 60)) / (1000 * 60)
+  );
 
-    return { days, hours };
+    return { days, hours, minutes };
+};
+
+  // 마감 시간이 지났는지 확인하는 함수
+  const isPastDeadline = (deadline) => {
+    return new Date() > new Date(deadline);
   };
 
   const goEduContent = (contentId) => {
@@ -65,10 +73,15 @@ const SearchApp = () => {
           >
             <img src={content.companyImg} alt="logo" />
             <div>
-              {!!content.deadLine && <span className="event">단기 이벤트</span>}
+              {!!content.deadLine && !isPastDeadline(content.deadLine) && (
+                <span className="event">단기 이벤트</span>
+              )}
+              {isPastDeadline(content.deadLine) && (
+                <span className="event-closed">* 본 이벤트는 마감된 이벤트입니다.</span>
+              )}
               <p className="content-company">[ {content.companyName} ]</p>
               <p className="content-title">{content.title}</p>
-              {!!content.deadLine && (
+              {!!content.deadLine && !isPastDeadline(content.deadLine) && (
                 <span className="content-time">
                   마감까지 약 {getTimeRemaining(content.deadLine).days}일{" "}
                   {getTimeRemaining(content.deadLine).hours}시간 남음
