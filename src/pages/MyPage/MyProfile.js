a6365ce299b4671e97c257ff36ff1d60c3de2580
박서영(Tessa) <lilianapsy@naver.com>
2023-08-17 22:35:13 +0900

    Update MyProfile.js: 프로필- 작성한 게시글 수 보이기(진행중)

--- src/pages/MyPage/MyProfile.js
+++ src/pages/MyPage/MyProfile.js
@@ -1,15 +1,40 @@
-// 나의 프로필
-import { useRecoilValue } from "recoil";
-import { userInfoState, postsState } from "../../Data/User";
+import { useRecoilValue, useRecoilState } from "recoil";
+import axios from "axios";
+import { useState, useEffect } from "react";
 import "../../styles/MyPage/MyProfile.scss";
 import { userState1, asksState } from "../../Data/state";
+import { userInfoState } from "../../Data/User";
 
 const MyProfile = () => {
   const user = useRecoilValue(userState1); //실제 유저 데이터
-
+  const [numberOfPosts, setNumberOfPosts] = useState(0); // 추가된 state
   const userInfo = useRecoilValue(userInfoState); //임시 데이터
-  const posts = useRecoilValue(postsState);
   const asks = useRecoilValue(asksState);
+
+  useEffect(() => {
+    const config = {
+      headers: {
+        Authorization: "Bearer " + user.token,
+      },
+    };
+    console.log("useEffect called");  // useEffect가 호출되는지 확인
+  
+    const fetchUserPosts = async () => {
+      console.log("fetchUserPosts called");  // 함수가 호출되는지 확인
+      
+      try {
+        const response = await axios.get(`http://13.209.49.229:8080/api/v1/content/user?userId=${user.id}`, config);
+        console.log(response.data);  // API 응답 자체를 확인
+        console.log("Number of user's posts:", response.data.result.length);
+        
+      } catch (error) {
+        console.error("Error fetching user's posts:", error);
+      }
+    };
+  
+    fetchUserPosts();
+  }, [user]);
+  
 
   return (
     <div className="profile-container">
@@ -32,7 +57,7 @@ const MyProfile = () => {
         </div>
       </div>
       <p className="profile-bottom">
-        작성한 게시글 <span>{posts.length}</span>
+        작성한 게시글 <span>{numberOfPosts}</span>
       </p>
       <p className="profile-bottom">
         작성한 문의글 <span>{asks.length}</span>
